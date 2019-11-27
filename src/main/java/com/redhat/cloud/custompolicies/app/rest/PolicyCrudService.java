123844036fcd9eadadb3bd23bc4e4cbbe178e549
Heiko W. Rupp <hrupp@redhat.com>
2019-11-27 17:14:42 +0100

    Bump Quarkus to 1.0.0.Final and supply OpenApi
    Signed-off-by: Heiko W. Rupp <hrupp@redhat.com>

--- src/main/java/com/redhat/cloud/custompolicies/app/rest/PolicyCrudService.java
+++ src/main/java/com/redhat/cloud/custompolicies/app/rest/PolicyCrudService.java
@@ -38,6 +38,12 @@ import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.hibernate.exception.ConstraintViolationException;
 
@@ -59,8 +65,12 @@ public class PolicyCrudService {
   @Context
   UriInfo uriInfo;
 
+  @Operation(summary = "Return all policies for a given account")
   @GET
   @Path("/{customer}")
+  @APIResponse(responseCode = "404", description = "No policies found for customer")
+  @APIResponse(responseCode = "200", description = "Policies found", content =
+                 @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = Policy.class)))
   public Response getPoliciesForCustomer(@PathParam("customer") String customer) {
 
     ResponseBuilder builder ;
@@ -78,6 +88,13 @@ public class PolicyCrudService {
     return builder.build();
   }
 
+  @Operation(summary = "Persist a passed policy for the given account")
+  @APIResponses({
+      @APIResponse(responseCode = "500", description = "No policy provided or internal error"),
+      @APIResponse(responseCode = "400", description = "Policy validation failed"),
+      @APIResponse(responseCode = "409", description = "Persisting failed"),
+      @APIResponse(responseCode = "204", description = "Policy persisted")
+                })
   @POST
   @Path("/{customer}")
   @Transactional
@@ -122,8 +139,12 @@ public class PolicyCrudService {
 
   }
 
+  @Operation(summary = "Retrieve a single policy for a customer by its id")
   @GET
   @Path("/{customer}/policy/{id}")
+  @APIResponse(responseCode = "200", description = "Policy found", content =
+                 @Content(schema = @Schema(implementation = Policy.class)))
+  @APIResponse(responseCode = "404", description = "Policy not found")
   public Response getPolicy(@PathParam("customer") String customerId, @PathParam("id") Long policyId) {
     Policy policy = Policy.findById(customerId, policyId);
 
