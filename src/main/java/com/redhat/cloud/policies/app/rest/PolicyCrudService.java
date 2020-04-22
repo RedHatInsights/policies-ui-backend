f4de0516a9ccccb9dafabc09e30e4bb4d406d59a
Heiko W. Rupp <hrupp@redhat.com>
2020-04-22 11:41:05 +0200

    Introduce explicit timeouts for calls to other services.  This way the user on the UI does not need to wait for too long if they are in a odd state.
    Signed-off-by: Heiko W. Rupp <hrupp@redhat.com>

--- src/main/java/com/redhat/cloud/policies/app/rest/PolicyCrudService.java
+++ src/main/java/com/redhat/cloud/policies/app/rest/PolicyCrudService.java
@@ -34,16 +34,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.Response;
@@ -427,7 +418,8 @@ public class PolicyCrudService {
 
   private Msg getEngineExceptionMsg(Exception e) {
     Msg msg;
-    if (e instanceof RuntimeException && e.getCause() instanceof ConnectException) {
+    if (e instanceof RuntimeException && e.getCause() instanceof ConnectException
+        || e instanceof ProcessingException) {
       msg = new Msg("Connection to backend-engine failed. Please retry later");
     } else {
       msg = new Msg(e.getMessage());
