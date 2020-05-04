44729529a892ea7edaf4f1a31c12948b2faf51c7
Heiko W. Rupp <hrupp@redhat.com>
2020-05-04 18:32:22 +0200

    Add missing import

--- src/main/java/com/redhat/cloud/policies/app/rest/PolicyCrudService.java
+++ src/main/java/com/redhat/cloud/policies/app/rest/PolicyCrudService.java
@@ -43,6 +43,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
