621f186def4bdc98579d56d4ef368a99628084a2
Yuval Lahav <ylahav@redhat.com>
2020-07-21 18:07:20 +0300

    added jenkinsfile-smoke.groovy

---
+++ Jenkinsfile-smoke.groovy
@@ -0,0 +1,18 @@
+/*
+ * Requires: https://github.com/RedHatInsights/insights-pipeline-lib
+ */
+
+@Library("github.com/RedHatInsights/insights-pipeline-lib@v3") _
+
+def options = [vaultEnabled: true, settingsFromGit: true, settingsGitPath: 'configs/policies-smoke-settings.yaml']
+
+if (env.CHANGE_ID) {
+        execSmokeTest (
+        ocDeployerBuilderPath: "policies/policies-ui-backend",
+        ocDeployerComponentPath: "policies/policies-ui-backend",
+        ocDeployerServiceSets: "policies,platform-mq, rbac",
+        iqePlugins: ["iqe-policies-plugin"],
+        pytestMarker: "policies_api_smoke",
+        options: options
+    )
+}
