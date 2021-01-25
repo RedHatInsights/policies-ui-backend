f30728f97f031b708988240adddc697602098383
Josejulio Martínez <jmartine@redhat.com>
2021-01-25 12:51:05 -0600

    POL-379 Add type ahead for new facts: tuned_profile, selinux_config_file and selinux_current_mode (#174)

---
+++ src/main/resources/db/migration/V8.sql
@@ -0,0 +1,7 @@
+insert into fact VALUES(34, 'facts.sap_system', 'STRING');
+insert into fact VALUES(35, 'facts.sap_sids', 'STRING');
+insert into fact VALUES(36, 'facts.sap_instance_number', 'STRING');
+insert into fact VALUES(37, 'facts.sap_version', 'STRING');
+insert into fact VALUES(38, 'facts.tuned_profile', 'STRING');
+insert into fact VALUES(39, 'facts.selinux_config_file', 'STRING');
+insert into fact VALUES(40, 'facts.selinux_current_mode', 'STRING');
