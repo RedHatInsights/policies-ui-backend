9d5ab435858a529746f53726b33a053977127872
Josejulio Martínez <jmartine@redhat.com>
2021-07-08 12:25:42 -0500

    POL-447 Add new facts  - installed_packages_delta  - cpu_model  - dnf_modules  - operating_system

---
+++ src/main/resources/db/migration/V9.0.1__POL-447_POL-468-add-facts.sql
@@ -0,0 +1,4 @@
+insert into fact VALUES(41, 'facts.installed_packages_delta', 'LIST');
+insert into fact VALUES(42, 'facts.cpu_model', 'STRING');
+insert into fact VALUES(43, 'facts.dnf_modules', 'LIST');
+insert into fact VALUES(44, 'facts.operating_system', 'STRING');
