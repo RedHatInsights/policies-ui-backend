0660756d795a963574edd1b7f61838b5edf9f6f0
Bennet Schulz <mail@bennet-schulz.de>
2021-07-27 08:49:09 +0200

    Move junit testcase dummy in pr_check

--- .rhcicd/build_deploy.sh
+++ .rhcicd/build_deploy.sh
@@ -36,14 +36,3 @@ then
     docker --config="$DOCKER_CONF" tag "${IMAGE}:${IMAGE_TAG}" "${IMAGE}:latest"
     docker --config="$DOCKER_CONF" push "${IMAGE}:latest"
 fi
-
-if [ "$1" = "testrun" ];
-then
-    # Until test results produce a junit XML file, create a dummy result file so Jenkins will pass
-    mkdir -p $WORKSPACE/artifacts
-    cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
-    <testsuite tests="1">
-        <testcase classname="dummy" name="dummytest"/>
-    </testsuite>
-EOF
-fi
--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -3,3 +3,11 @@
 set -exv
 
 /bin/bash build_deploy.sh testrun
+
+# Until test results produce a junit XML file, create a dummy result file so Jenkins will pass
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > ${WORKSPACE}/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
