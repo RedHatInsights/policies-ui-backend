009cedd72b67dfdf199d2aae4623aadf4339c07e
Bennet Schulz <mail@bennet-schulz.de>
2021-07-30 16:16:28 +0200

    POL-549 Use the old pr_check script again

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -2,15 +2,7 @@
 
 set -exv
 
-IMAGE="quay.io/cloudservices/policies-ui-backend"
-IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-
-DOCKER_CONF="$PWD/.docker"
-mkdir -p "$DOCKER_CONF"
-
-docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" . -f src/main/docker/Dockerfile-build.jvm
-
-#/bin/bash .rhcicd/build_deploy.sh testrun
+/bin/bash .rhcicd/build_deploy.sh testrun
 
 # Until test results produce a junit XML file, create a dummy result file so Jenkins will pass
 mkdir -p $WORKSPACE/artifacts
