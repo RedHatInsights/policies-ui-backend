476483f01502cf15424fea19479f9d160710909d
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2021-10-11 14:51:57 +0200

    Add HTTP protocol to policies-engine URL from Clowder (#294)

--- src/main/java/com/redhat/cloud/policies/app/rest/PolicyCrudService.java
+++ src/main/java/com/redhat/cloud/policies/app/rest/PolicyCrudService.java
@@ -172,8 +172,8 @@ public class PolicyCrudService {
         }
         // TODO This is needed to fix a Clowder issue. Remove it ASAP.
         if (engineUrlFromClowder.isPresent()) {
-            log.info("Overriding the policies-engine URL with the config value from Clowder: " + engineUrlFromClowder.get());
-            System.setProperty(ENGINE_URL_KEY, engineUrlFromClowder.get());
+            log.info("Overriding the policies-engine URL with the config value from Clowder: http://" + engineUrlFromClowder.get());
+            System.setProperty(ENGINE_URL_KEY, "http://" + engineUrlFromClowder.get());
         }
         String engineUrl = ConfigProvider.getConfig().getValue(ENGINE_URL_KEY, String.class);
         log.info(ENGINE_URL_KEY + " is set to " + engineUrl);
