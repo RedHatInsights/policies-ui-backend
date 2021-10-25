b452d024f0bfb08d3122457805ea40670a24fd12
Gwenneg Lepage <gwenneg@users.noreply.github.com>
2021-10-25 09:45:01 +0200

    Use RBAC URL from Clowder (#308)

--- src/main/java/com/redhat/cloud/policies/app/ClowderConfigInitializer.java
+++ src/main/java/com/redhat/cloud/policies/app/ClowderConfigInitializer.java
@@ -13,15 +13,24 @@ public class ClowderConfigInitializer {
 
     private static final Logger LOGGER = Logger.getLogger(ClowderConfigInitializer.class);
     private static final String ENGINE_URL_KEY = "engine/mp-rest/url";
+    private static final String RBAC_URL_KEY = "rbac/mp-rest/url";
 
-    @ConfigProperty(name = "clowder.endpoints.policies-engine")
+    @ConfigProperty(name = "clowder.endpoints.policies-engine-policies-engine")
     Optional<String> policiesEngineClowderEndpoint;
+
+    @ConfigProperty(name = "clowder.endpoints.rbac-service")
+    Optional<String> rbacClowderEndpoint;
 
     void init(@Observes StartupEvent event) {
         if (policiesEngineClowderEndpoint.isPresent()) {
             String engineUrl = "http://" + policiesEngineClowderEndpoint.get();
             LOGGER.infof("Overriding the policies-engine URL with the config value from Clowder: %s", engineUrl);
             System.setProperty(ENGINE_URL_KEY, engineUrl);
+        }
+        if (rbacClowderEndpoint.isPresent()) {
+            String rbacUrl = "http://" + rbacClowderEndpoint.get();
+            LOGGER.infof("Overriding the RBAC URL with the config value from Clowder: %s", rbacUrl);
+            System.setProperty(RBAC_URL_KEY, rbacUrl);
         }
     }
 }
