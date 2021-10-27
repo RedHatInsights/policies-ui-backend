09a53dddf87e5454db9f005214c6e3cf26a4c210
Bennet Schulz <mail@bennet-schulz.de>
2021-10-27 13:29:48 +0200

    POL-582 Remove star import

--- src/test/java/com/redhat/cloud/policies/app/model/validation/ActionValidatorTest.java
+++ src/test/java/com/redhat/cloud/policies/app/model/validation/ActionValidatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 class ActionValidatorTest {
 
