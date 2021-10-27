8cb294429ef1058035b987103560be031f4382d8
Bennet Schulz <mail@bennet-schulz.de>
2021-10-27 14:33:56 +0200

    POL-582 Remove unused import

--- src/main/java/com/redhat/cloud/policies/app/model/validation/ActionValidator.java
+++ src/main/java/com/redhat/cloud/policies/app/model/validation/ActionValidator.java
@@ -16,7 +16,6 @@
  */
 package com.redhat.cloud.policies.app.model.validation;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
