6df46194a861039a6a5861d6942bd5dd5e4d7395
Josejulio Martínez <jmartine@redhat.com>
2021-11-18 13:28:05 -0700

    Prints env flags in use

--- src/main/java/com/redhat/cloud/policies/app/EnvironmentFlags.java
+++ src/main/java/com/redhat/cloud/policies/app/EnvironmentFlags.java
@@ -1,11 +1,31 @@
 package com.redhat.cloud.policies.app;
 
+import io.quarkus.runtime.Startup;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
 
-@ApplicationScoped
+@Startup
 public class EnvironmentFlags {
+
+    private static final Logger LOG = Logger.getLogger(EnvironmentFlags.class);
+
+    @PostConstruct
+    public void init() {
+        try {
+            LOG.info("Environment flags in use:");
+            for (Field field: EnvironmentFlags.class.getDeclaredFields()) {
+                ConfigProperty configAnnotation = field.getAnnotation(ConfigProperty.class);
+                if (configAnnotation != null) {
+                    LOG.infof("  - %s: %s", configAnnotation.name(), field.get(this));
+                }
+            }
+        } catch (IllegalAccessException iae) {
+            throw new RuntimeException("Error getting flags while starting.", iae);
+        }
+    }
 
     @ConfigProperty(name = "flag.is-fedramp", defaultValue = "false")
     Boolean isFedramp;
