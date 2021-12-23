0913f01450a0fb258a7ebf5efd2707fc1a9318b8
Bennet Schulz <mail@bennet-schulz.de>
2021-12-23 13:13:43 +0100

    POL-595 Remove unused import

--- src/main/java/com/redhat/cloud/policies/app/health/StatusEndpoint.java
+++ src/main/java/com/redhat/cloud/policies/app/health/StatusEndpoint.java
@@ -19,7 +19,6 @@ package com.redhat.cloud.policies.app.health;
 import com.redhat.cloud.policies.app.StuffHolder;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
