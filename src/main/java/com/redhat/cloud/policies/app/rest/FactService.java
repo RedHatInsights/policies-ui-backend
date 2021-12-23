859a2f188d339c84e8c9b97598006db0a5799c62
Bennet Schulz <mail@bennet-schulz.de>
2021-12-23 09:56:54 +0100

    POL-595 Remove unused import

--- src/main/java/com/redhat/cloud/policies/app/rest/FactService.java
+++ src/main/java/com/redhat/cloud/policies/app/rest/FactService.java
@@ -27,7 +27,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import io.micrometer.core.annotation.Timed;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
