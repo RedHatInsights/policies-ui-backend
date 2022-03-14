8ee8b31a223a62b53d568b767028c7cf5647ccb7
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-03-14 12:04:23 -0600

    POL-593 Adding ansible facts

---
+++ src/main/resources/db/migration/V14__POL-593-adding-ansible-facts.sql
@@ -0,0 +1,5 @@
+insert into fact VALUES(45, 'facts.ansible', 'LIST');
+insert into fact VALUES(46, 'facts.ansible.tower_version', 'STRING');
+insert into fact VALUES(47, 'facts.ansible.hub_version', 'STRING');
+insert into fact VALUES(48, 'facts.ansible.catalog_worker_version', 'STRING');
+insert into fact VALUES(49, 'facts.ansible.controller_version', 'STRING');
