abe4ca26d0d610bbc48ee6d4e2a7c7e74475eeb0
Gwenneg Lepage <glepage@redhat.com>
2022-05-12 19:03:56 +0200

    Add missing annotation (not mandatory though)

--- src/main/java/com/redhat/cloud/policies/app/lightweight/AccountLatestUpdateRepository.java
+++ src/main/java/com/redhat/cloud/policies/app/lightweight/AccountLatestUpdateRepository.java
@@ -3,6 +3,7 @@ package com.redhat.cloud.policies.app.lightweight;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
 import static java.time.ZoneOffset.UTC;
@@ -13,6 +14,7 @@ public class AccountLatestUpdateRepository {
     @Inject
     EntityManager entityManager;
 
+    @Transactional
     public void setLatestToNow(String accountId) {
         String sql = "INSERT INTO account_latest_update (account_id, latest) SELECT :accountId, :latest " +
                 "ON CONFLICT (account_id) DO UPDATE SET latest = :latest";
