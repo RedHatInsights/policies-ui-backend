03a140bdc365afb9b73773ccc8fd8ef31c219328
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-05-16 07:17:20 -0600

    - Rebase and upgrade script name  - Consider the account-id when updating the last-triggered  - Adding test for the account-id case

--- src/test/java/com/redhat/cloud/policies/app/PoliciesLastTriggeredTest.java
+++ src/test/java/com/redhat/cloud/policies/app/PoliciesLastTriggeredTest.java
@@ -7,6 +7,7 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.hibernate.CacheMode;
 import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @QuarkusTest
 @QuarkusTestResource(TestLifecycleManager.class)
@@ -25,7 +27,8 @@ class PoliciesLastTriggeredTest {
     @Inject
     Session session;
 
-    private final String accountId = "account-id";
+    private final String testAccountId = "lt-test-account-id";
+    private final String otherTestAccountId = "lt-test-2-account-id";
 
     @BeforeEach
     @Transactional
@@ -37,19 +40,19 @@ class PoliciesLastTriggeredTest {
     @Test
     @TestTransaction
     void testPolicyLastTriggeredIs0ByDefault() {
-        Policy policy = createPolicy();
+        Policy policy = createPolicy(testAccountId, UUID.randomUUID());
         assertEquals(0, policy.getLastTriggered());
     }
 
     @Test
     @TestTransaction
     void testPolicyLastTriggeredShouldUpdateToGreatestHistory() {
-        Policy policy = createPolicy();
+        Policy policy = createPolicy(testAccountId, UUID.randomUUID());
         long lastTime = Instant.now().toEpochMilli();
 
-        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofHours(1L)).toEpochMilli(), policy.id);
-        createPoliciesHistoryEntry(lastTime, policy.id);
-        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofDays(28L)).toEpochMilli(), policy.id);
+        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofHours(1L)).toEpochMilli(), policy.id, policy.customerid);
+        createPoliciesHistoryEntry(lastTime, policy.id, policy.customerid);
+        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofDays(28L)).toEpochMilli(), policy.id, policy.customerid);
 
         policy = getPolicy(policy.id);
 
@@ -58,17 +61,25 @@ class PoliciesLastTriggeredTest {
 
     @Test
     @TestTransaction
-    void testPolicyLastTriggeredAreSetById() {
-        Policy policy = createPolicy();
-        Policy policy2 = createPolicy();
+    void testPolicyLastTriggeredAreSetByIdAndAccount() {
+        UUID uuid1 = UUID.randomUUID();
+        UUID uuid2 = UUID.randomUUID();
 
-        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofHours(1L)).toEpochMilli(), policy.id);
-        createPoliciesHistoryEntry(Instant.now().toEpochMilli(), policy.id);
-        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofDays(28L)).toEpochMilli(), policy.id);
+        Policy policy = createPolicy(testAccountId, uuid1);
+        Policy policy2 = createPolicy(testAccountId, uuid2);
+        Policy policy3 = createPolicy(otherTestAccountId, uuid1);
 
+        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofHours(1L)).toEpochMilli(), policy.id, policy.customerid);
+        createPoliciesHistoryEntry(Instant.now().toEpochMilli(), policy.id, policy.customerid);
+        createPoliciesHistoryEntry(Instant.now().minus(Duration.ofDays(28L)).toEpochMilli(), policy.id, policy.customerid);
+
+        policy = getPolicy(policy.id);
         policy2 = getPolicy(policy2.id);
+        policy3 = getPolicy(policy3.id);
 
+        assertNotEquals(0, policy.getLastTriggered());
         assertEquals(0, policy2.getLastTriggered());
+        assertEquals(0, policy3.getLastTriggered());
     }
 
     private Policy getPolicy(UUID policyId) {
@@ -79,7 +90,7 @@ class PoliciesLastTriggeredTest {
         return session.find(Policy.class, policyId);
     }
 
-    private Policy createPolicy() {
+    private Policy createPolicy(String accountId, UUID policyId) {
         Policy policy = new Policy();
         policy.id = UUID.randomUUID();
         policy.customerid = accountId;
@@ -92,7 +103,7 @@ class PoliciesLastTriggeredTest {
         return getPolicy(policy.id);
     }
 
-    private void createPoliciesHistoryEntry(long instant, UUID policyId) {
+    private void createPoliciesHistoryEntry(long instant, UUID policyId, String accountId) {
         PoliciesHistoryEntry historyEntry = new PoliciesHistoryEntry();
         historyEntry.setId(UUID.randomUUID());
         historyEntry.setTenantId(accountId);
