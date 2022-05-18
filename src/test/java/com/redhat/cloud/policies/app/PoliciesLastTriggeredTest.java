3954df273ff7947b371fc23a21c05153fdfd75ca
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-05-18 08:50:05 -0600

    Adds transactions on the methods and not on the test

--- src/test/java/com/redhat/cloud/policies/app/PoliciesLastTriggeredTest.java
+++ src/test/java/com/redhat/cloud/policies/app/PoliciesLastTriggeredTest.java
@@ -2,10 +2,10 @@ package com.redhat.cloud.policies.app;
 
 import com.redhat.cloud.policies.app.model.Policy;
 import com.redhat.cloud.policies.app.model.history.PoliciesHistoryEntry;
-import io.quarkus.test.TestTransaction;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,22 +28,24 @@ class PoliciesLastTriggeredTest {
     private final String testAccountId = "lt-test-account-id";
     private final String otherTestAccountId = "lt-test-2-account-id";
 
-    @BeforeEach
     @Transactional
+    @BeforeEach
     void beforeEach() {
         session.createQuery("DELETE FROM PoliciesHistoryEntry")
+                .executeUpdate();
+        session.createQuery("DELETE FROM Policy where customerid = :customerid1 OR customerid= :customerid2")
+                .setParameter("customerid1", testAccountId)
+                .setParameter("customerid2", otherTestAccountId)
                 .executeUpdate();
     }
 
     @Test
-    @TestTransaction
     void testPolicyLastTriggeredIs0ByDefault() {
         Policy policy = createPolicy(testAccountId, UUID.randomUUID());
         assertEquals(0, policy.getLastTriggered());
     }
 
     @Test
-    @TestTransaction
     void testPolicyLastTriggeredShouldUpdateToGreatestHistory() {
         Policy policy = createPolicy(testAccountId, UUID.randomUUID());
         long lastTime = Instant.now().toEpochMilli();
@@ -58,7 +60,6 @@ class PoliciesLastTriggeredTest {
     }
 
     @Test
-    @TestTransaction
     void testPolicyLastTriggeredAreSetById() {
         Policy policy = createPolicy(testAccountId, UUID.randomUUID());
         Policy policy2 = createPolicy(testAccountId, UUID.randomUUID());
@@ -78,34 +79,41 @@ class PoliciesLastTriggeredTest {
     }
 
     private Policy getPolicy(UUID policyId) {
-        // The store procedure does not play well with hibernate.
-        // This should not be a problem with real data as they are updated in separated transactions
-        session.flush();
-        session.clear();
         return session.find(Policy.class, policyId);
     }
 
+    @Transactional
     private Policy createPolicy(String accountId, UUID policyId) {
-        Policy policy = new Policy();
-        policy.id = policyId;
-        policy.customerid = accountId;
-        policy.name = "Policy: " +  policy.id.toString();
-        policy.actions = "";
-        policy.conditions = "foo";
-        policy.setMtimeToNow();
+        Transaction txn = session.beginTransaction();
+        try {
+            Policy policy = new Policy();
+            policy.id = policyId;
+            policy.customerid = accountId;
+            policy.name = "Policy: " +  policy.id.toString();
+            policy.actions = "";
+            policy.conditions = "foo";
+            policy.setMtimeToNow();
 
-        session.persist(policy);
-        return getPolicy(policy.id);
+            session.persist(policy);
+            return getPolicy(policy.id);
+        } finally {
+            txn.commit();
+        }
     }
 
+    @Transactional
     private void createPoliciesHistoryEntry(long instant, UUID policyId, String accountId) {
-        PoliciesHistoryEntry historyEntry = new PoliciesHistoryEntry();
-        historyEntry.setId(UUID.randomUUID());
-        historyEntry.setTenantId(accountId);
-        historyEntry.setPolicyId(policyId.toString());
-        historyEntry.setCtime(instant);
-        session.persist(historyEntry);
-        session.flush();
+        Transaction txn = session.beginTransaction();
+        try {
+            PoliciesHistoryEntry historyEntry = new PoliciesHistoryEntry();
+            historyEntry.setId(UUID.randomUUID());
+            historyEntry.setTenantId(accountId);
+            historyEntry.setPolicyId(policyId.toString());
+            historyEntry.setCtime(instant);
+            session.persist(historyEntry);
+        } finally {
+            txn.commit();
+        }
     }
 
 }
