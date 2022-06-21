76b9fe3d97f31d8dda2f44a6d4193c131fdbd248
Bennet Schulz <mail@bennet-schulz.de>
2022-06-21 08:31:59 +0200

    POL-650 Remove flyway script and revert the other one

--- src/main/resources/db/migration/V19__POL-650_add_org_id.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE policies_history
-  ADD COLUMN org_id text;
-
-CREATE INDEX ix_policies_history_org_id ON public.policies_history (org_id);
-
-ALTER TABLE policy
-  ADD COLUMN org_id text;
-
-CREATE INDEX ix_policy_org_id ON public.policy (org_id);
