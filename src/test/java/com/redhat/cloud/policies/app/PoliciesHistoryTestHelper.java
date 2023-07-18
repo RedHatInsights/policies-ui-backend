a46a6259f57472268e55aeb715f480b44f734d2d
Viliam Krizan <vkrizan@redhat.com>
2023-07-18 15:41:48 +0200

    feat(repository): lookup by host groups
    Add find lookups and count by host group ids for policies hitory entries.
    
    RHINENG-1161

--- src/test/java/com/redhat/cloud/policies/app/PoliciesHistoryTestHelper.java
+++ src/test/java/com/redhat/cloud/policies/app/PoliciesHistoryTestHelper.java
@@ -1,11 +1,17 @@
 package com.redhat.cloud.policies.app;
 
 import com.redhat.cloud.policies.app.model.history.PoliciesHistoryEntry;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+
 import org.hibernate.Session;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+
+import java.util.List;
 import java.util.UUID;
 
 @ApplicationScoped
@@ -16,6 +22,20 @@ public class PoliciesHistoryTestHelper {
 
     @Transactional
     public PoliciesHistoryEntry createPoliciesHistoryEntry(String tenantId, String orgId, UUID policyId, String hostId, String hostName, long ctime) {
+        PoliciesHistoryEntry historyEntry = newPoliciesHistoryEntry(tenantId, orgId, policyId, hostId, hostName, ctime);
+        session.persist(historyEntry);
+        return historyEntry;
+    }
+
+    @Transactional
+    public PoliciesHistoryEntry createPoliciesHistoryEntry(String tenantId, String orgId, UUID policyId, String hostId, String hostName, List<UUID> hostGroupIds, long ctime) {
+        PoliciesHistoryEntry historyEntry = newPoliciesHistoryEntry(tenantId, orgId, policyId, hostId, hostName, ctime);
+        historyEntry.setHostGroups(hostGroupIdsToJsonArray(hostGroupIds));
+        session.persist(historyEntry);
+        return historyEntry;
+    }
+
+    PoliciesHistoryEntry newPoliciesHistoryEntry(String tenantId, String orgId, UUID policyId, String hostId, String hostName, long ctime) {
         PoliciesHistoryEntry historyEntry = new PoliciesHistoryEntry();
         historyEntry.setId(UUID.randomUUID());
         historyEntry.setTenantId(tenantId);
@@ -24,7 +44,14 @@ public class PoliciesHistoryTestHelper {
         historyEntry.setHostId(hostId);
         historyEntry.setHostName(hostName);
         historyEntry.setCtime(ctime);
-        session.persist(historyEntry);
         return historyEntry;
+    }
+
+    JsonArray hostGroupIdsToJsonArray(List<UUID> hostGroupIds) {
+        JsonArray jsonArray = new JsonArray();
+        for (UUID hostGroupId : hostGroupIds) {
+            jsonArray.add(JsonObject.of("id", hostGroupId.toString()));
+        }
+        return jsonArray;
     }
 }
--- src/test/java/com/redhat/cloud/policies/app/model/history/PoliciesHistoryRepositoryTest.java
+++ src/test/java/com/redhat/cloud/policies/app/model/history/PoliciesHistoryRepositoryTest.java
@@ -3,6 +3,7 @@ package com.redhat.cloud.policies.app.model.history;
 import com.redhat.cloud.policies.app.PoliciesHistoryTestHelper;
 import com.redhat.cloud.policies.app.TestLifecycleManager;
 import com.redhat.cloud.policies.app.model.pager.Pager;
+
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.BeforeAll;
 import javax.inject.Inject;
 import java.util.List;
 import java.util.UUID;
+import java.util.ArrayList;
 
 import static com.redhat.cloud.policies.app.model.filter.Filter.Operator.EQUAL;
 import static com.redhat.cloud.policies.app.model.filter.Filter.Operator.LIKE;
@@ -28,14 +30,21 @@ class PoliciesHistoryRepositoryTest {
 
     private static final String TENANT_ID_1 = "tenant-id-1";
     private static final String TENANT_ID_2 = "tenant-id-2";
+    private static final String TENANT_ID_3 = "tenant-id-3";
     private static final String ORG_ID_1 = "org-id-1";
     private static final String ORG_ID_2 = "org-id-2";
+    private static final String ORG_ID_3 = "org-id-3";
     private static final UUID POLICY_ID_1 = UUID.randomUUID();
     private static final UUID POLICY_ID_2 = UUID.randomUUID();
+    private static final UUID POLICY_ID_3 = UUID.randomUUID();
     private static final String HOST_ID_1 = "host-id-1";
     private static final String HOST_ID_2 = "host-id-2";
+    private static final String HOST_ID_3 = "host-id-4";
     private static final String HOST_NAME_1 = "host-name-1";
     private static final String HOST_NAME_2 = "host-name-2";
+    private static final String HOST_NAME_3 = "host-name-4";
+    private static final UUID GROUP_ID_1 = UUID.randomUUID();
+    private static final UUID GROUP_ID_2 = UUID.randomUUID();
 
     @Inject
     PoliciesHistoryTestHelper helper;
@@ -53,7 +62,13 @@ class PoliciesHistoryRepositoryTest {
             helper.createPoliciesHistoryEntry(TENANT_ID_2, ORG_ID_2, POLICY_ID_2, HOST_ID_1, HOST_NAME_1, 3L),
             helper.createPoliciesHistoryEntry(TENANT_ID_2, ORG_ID_2, POLICY_ID_2, HOST_ID_1, HOST_NAME_2, 3L),
             helper.createPoliciesHistoryEntry(TENANT_ID_2, ORG_ID_2, POLICY_ID_2, HOST_ID_2 + "-foo", HOST_NAME_2, 4L),
-            helper.createPoliciesHistoryEntry(TENANT_ID_2, ORG_ID_2, POLICY_ID_2, HOST_ID_2, "bar-" + HOST_NAME_2, 5L)
+            helper.createPoliciesHistoryEntry(TENANT_ID_2, ORG_ID_2, POLICY_ID_2, HOST_ID_2, "bar-" + HOST_NAME_2, 5L),
+
+            helper.createPoliciesHistoryEntry(TENANT_ID_3, ORG_ID_3, POLICY_ID_3, HOST_ID_1, HOST_NAME_1,1L),
+            helper.createPoliciesHistoryEntry(TENANT_ID_3, ORG_ID_3, POLICY_ID_3, HOST_ID_3, HOST_NAME_3,
+                                              List.of(GROUP_ID_1), 1L),
+            helper.createPoliciesHistoryEntry(TENANT_ID_3, ORG_ID_3, POLICY_ID_3, HOST_ID_2, HOST_NAME_2,
+                                              List.of(GROUP_ID_2), 1L)
         );
     }
 
@@ -143,5 +158,85 @@ class PoliciesHistoryRepositoryTest {
                 .addSort("name", Ascending)
                 .addSort("ctime", Descending).build();
         repository.find(ORG_ID_2, POLICY_ID_2, pager);
+    }
+
+    @Test
+    void testHostGroupsNoResult() {
+        Pager pager = Pager.builder().build();
+        long count = repository.count(ORG_ID_3, List.of(UUID.randomUUID()), POLICY_ID_3, pager);
+        assertEquals(0, count);
+        assertTrue(repository.find(ORG_ID_3, List.of(UUID.randomUUID()), POLICY_ID_3, pager).isEmpty());
+    }
+
+    @Test
+    void testHostGroupsWithNoGroup() {
+        Pager pager = Pager.builder().build();
+
+        List<UUID> hostGroupIds = new ArrayList<UUID>();
+        // Passing a null within hostGroupIds to lookup entries
+        // of systems that are not in a group.
+        // Default value of host_groups is `[]` empty JSONB array.
+        hostGroupIds.add((UUID) null);
+        assertEquals(4, repository.count(ORG_ID_2, hostGroupIds, POLICY_ID_2, pager));
+        assertEquals(1, repository.count(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager));
+
+        List<PoliciesHistoryEntry> entries = repository.find(ORG_ID_2, hostGroupIds, POLICY_ID_2, pager);
+        assertEquals(4, entries.size());
+        for (PoliciesHistoryEntry entry : entries) {
+            assertEquals(0, entry.getHostGroups().size(), entry.toString());
+        }
+
+        entries = repository.find(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager);
+        assertEquals(1, entries.size());
+        assertEquals(0, entries.get(0).getHostGroups().size());
+    }
+
+    @Test
+    void testHostGroupsOneGroup() {
+        Pager pager = Pager.builder().build();
+        List<UUID> hostGroupIds = List.of(GROUP_ID_1);
+        assertEquals(1, repository.count(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager));
+        assertEquals(0, repository.count(ORG_ID_2, hostGroupIds, POLICY_ID_2, pager));
+
+        List<PoliciesHistoryEntry> entries = repository.find(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager);
+        assertEquals(1, entries.size());
+        assertEquals(1, entries.get(0).getHostGroups().size());
+        assertEquals(GROUP_ID_1.toString(), entries.get(0).getHostGroups().getJsonObject(0).getString("id"));
+    }
+
+    @Test
+    void testHostGroupsTwoGroupsLookup() {
+        Pager pager = Pager.builder().build();
+        List<UUID> hostGroupIds = List.of(UUID.randomUUID(), GROUP_ID_1);
+        assertEquals(1, repository.count(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager));
+        assertEquals(0, repository.count(ORG_ID_2, hostGroupIds, POLICY_ID_2, pager));
+
+        List<PoliciesHistoryEntry> entries = repository.find(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager);
+        assertEquals(1, entries.size());
+        assertEquals(1, entries.get(0).getHostGroups().size());
+        assertEquals(GROUP_ID_1.toString(), entries.get(0).getHostGroups().getJsonObject(0).getString("id"));
+    }
+
+    @Test
+    void testostGroupsCombined() {
+        Pager pager = Pager.builder().build();
+
+        List<UUID> hostGroupIds = new ArrayList<UUID>();
+        // Passing a null within hostGroupIds to lookup entries
+        // of systems that are not in a group.
+        // Default value of host_groups is `[]` empty JSONB array.
+        hostGroupIds.add((UUID) null);
+        hostGroupIds.add(GROUP_ID_1);
+
+        assertEquals(2, repository.count(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager));
+
+        List<PoliciesHistoryEntry> entries = repository.find(ORG_ID_3, hostGroupIds, POLICY_ID_3, pager);
+        assertEquals(2, entries.size());
+        for (PoliciesHistoryEntry entry : entries) {
+            if (entry.getHostGroups().size() > 0) {
+                assertEquals(GROUP_ID_1.toString(), entry.getHostGroups().getJsonObject(0).getString("id"),
+                             entry.toString());
+            }
+        }
     }
 }
