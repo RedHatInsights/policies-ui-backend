284bc7c1f3970330242d09c87676e122aaae71b0
casey-williams-rh <caswilli@redhat.com>
2023-09-21 12:10:49 -0400

    Update Security-Compliance Tag Naming Convention (#572)

--- .rhcicd/build_deploy.sh
+++ .rhcicd/build_deploy.sh
@@ -17,7 +17,7 @@ mkdir -p "$DOCKER_CONF"
 
 IMAGE="quay.io/cloudservices/policies-ui-backend"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)"
+SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
 
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
