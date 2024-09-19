d06f9671bccec590f98e0647d6b40f3174a3b038
PJ Richardson <prichard@redhat.com>
2024-09-19 14:42:13 -0400

    Add export of IQE_CJI_NAME

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -34,6 +34,7 @@ mkdir -p $WORKSPACE/artifacts
 
 # adding floorist tests.
 export COMPONENT_NAME="policies-engine"
+export IQE_CJI_NAME="floorist"
 # I also need to pass in FLOORPLAN_2_TEST.
 export IQE_ENV_VARS="FLOORPLAN_TO_TEST=policies-backend-hms"
 export IQE_PLUGINS="floorist"
